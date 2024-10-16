d548c7e459fe91a5993d9336897d84fb30d9161f
Kurt Punzalan <kurtbriandaine.1225@gmail.com>
2024-10-16 14:58:42 +0800

    feat(front/back): added auth and landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,59 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
-import { Sidebar } from "~/app/_components/Sidebar";
+import { Sidebar } from "~/app/_components/sidebar";
+import LoginButton from "./_components/button";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
 
   void api.post.getLatest.prefetch();
 
+  const session = await getServerAuthSession();
+
   return (
     <HydrateClient>
-      <main className=""></main>
-      {/* <Sidebar /> */}
+      <main className="m-7 flex flex-col justify-around">
+        {/* LOGO AND COMPANY NAME */}
+        <div className="mb-0 flex items-center gap-4">
+          <Image
+            src={"/assets/mc-company-logo.png"}
+            width={50}
+            height={50}
+            alt="Company Logo"
+            className="rounded-lg"
+          />
+
+          <p className="text-lg">MC Company</p>
+        </div>
+
+        {/* HERO SECTION */}
+        <div className="flex items-center justify-center gap-16">
+          <Image
+            src="/assets/hello.svg"
+            width={400}
+            height={400}
+            alt="Company Logo"
+            className="mb-4 rounded-lg" // Add margin below the image
+          />
+
+          <div className="flex flex-col gap-5 text-center">
+            <h1 className="text-center text-6xl font-bold text-white">
+              Hey, Team!
+            </h1>
+            <p className="max-w-xs text-xl text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            </p>
+          </div>
+        </div>
+
+        <LoginButton session={session} />
+        {/* <Sidebar /> */}
+        {/* <div></div> */}
+      </main>
     </HydrateClient>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,27 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        discord_black: "#313338",
+        discord_left: "#1E1F22",
+        discord_button: "#5863F8",
+        white: "#FFFFFF",
+
+        everyone: "#C9CDFB",
+        everyone_bg: "#4A4B72",
+
+        ceo: "#F1C40F",
+        ceo_bg: "#4C483F",
+
+        video_editor: "#3498DB",
+        video_editor_bg: "#43484A",
+
+        customer_service: "#E91E63",
+        customer_service_bg: "#563E3D",
+
+        funnel_builders: "#2FCC71",
+        funnel_builders_bg: "#444E3F",
+      }
     },
   },
   plugins: [],
