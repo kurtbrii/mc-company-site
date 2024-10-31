8733737a5ba7f207bfe6c8cd7c6678683b0130c1
Kurt Punzalan <kurtbriandaine.1225@gmail.com>
2024-10-31 16:35:16 +0800

    feat(front): added day in weekly-survey

--- src/app/weekly-survey/page.tsx
+++ src/app/weekly-survey/page.tsx
@@ -1,15 +1,28 @@
+"use client";
+
 import Sidebar from "../_components/sidebar";
 
 export default function name() {
+  const dateNow = new Date(Date.now()).toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+
   return (
     <div className="flex">
       {/* SIDEBAR */}
       <Sidebar />
 
       {/* DASHBOARD - TIME IN/TIME OUT */}
-      <div className="flex h-screen w-screen items-center justify-center">
-        <p>Weekly Survey</p>
-      </div>
+
+      {dateNow === "Friday" ? (
+        <div className="flex h-screen w-screen items-center justify-center">
+          <p>You </p>
+        </div>
+      ) : (
+        <div className="flex h-screen w-screen items-center justify-center">
+          <p>This section is not available until Friday.</p>
+        </div>
+      )}
     </div>
   );
 }
