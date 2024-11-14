5468057f664fd4a57d5925733235b3b8d17f6cbe
Kurt Punzalan <kurtbriandaine.1225@gmail.com>
2024-11-14 18:56:43 +0800

    feat(back): restrict members in navigating protected pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,17 +14,30 @@ export async function middleware(req: NextRequest, res: NextResponse) {
   };
 
   const session = await getSession({ req: requestForNextAuth });
+  const user = session?.user
 
+  // ! only logged in users can access (which is basically everything)
   if (!session) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
+  // ! current user in session (own time in, own bonus sheet and is not the CEO);
+  const currentURL = req.nextUrl.pathname
+
+  const notMyTimeInDetails = currentURL.startsWith('/time-in/') && currentURL.split('/')[2] !== user?.id // user navigates to time in details of other members
+  const notMyBonusDetails = currentURL.startsWith('/bonus-sheet/') && currentURL.split('/')[3] !== user?.id // user navigates to bonus sheet details of other members
+
+  const ceoURLs = [`/time-in`, '/bonus-sheet'] // paths that are only accessible to the CEO
+  const isUrlProtected = ceoURLs.includes(req.nextUrl.pathname)
+
+  if (user?.role !== 'CEO') {
+    if (notMyTimeInDetails || notMyBonusDetails || isUrlProtected) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+  }
   return NextResponse.next()
-
-
 }
 
-// Define the paths that the middleware applies to
 export const config = {
   runtime: 'nodejs',
   matcher: [
