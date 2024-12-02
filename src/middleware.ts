cf50c4fbfcdf6031f8812dc26ed4eb4c94a55c9f
Kurt Punzalan <kurtbriandaine.1225@gmail.com>
2024-12-02 14:44:13 +0800

    (fix: back) unclickable bonus sheet for other members

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,15 +21,15 @@ export async function middleware(req: NextRequest, res: NextResponse) {
   // ! current user in session (own time in, own bonus sheet and is not the CEO);
   const currentURL = req.nextUrl.pathname
 
-  const notMyTimeInDetails = currentURL.startsWith('/time-in') && currentURL.split('/')[2] !== user?.id // user navigates to time in details of other members
-  const notMyBonusDetails = currentURL.startsWith('/bonus-sheet') && (getBonusRole(user!.role) !== currentURL.split('/')[2] || currentURL.split('/')[3] !== user?.id) // user navigates to bonus sheet details of other members
+  const notMyTimeInDetails = currentURL.startsWith('/time-in/') && currentURL.split('/')[2] !== user?.id // user navigates to time in details of other members
+  const notMyBonusDetails = currentURL.startsWith('/bonus-sheet/') && (getBonusRole(user!.role) !== currentURL.split('/')[2] || currentURL.split('/')[3] !== user?.id) // user navigates to bonus sheet details of other members
 
 
   const ceoURLs = [`/time-in`, '/bonus-sheet'] // paths that are only accessible to the CEO
   const isUrlProtected = ceoURLs.includes(req.nextUrl.pathname)
 
   if (user?.role !== 'CEO' && user?.role !== "MANAGER") {
-    if (notMyTimeInDetails || notMyBonusDetails || currentURL === '/monthly-survey') {
+    if (notMyTimeInDetails || notMyBonusDetails || currentURL === '/monthly-survey' || currentURL === '/bonus-sheet') {
       return NextResponse.redirect(new URL("/dashboard", req.url));
     }
   }
